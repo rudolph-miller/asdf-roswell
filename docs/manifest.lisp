4e0bc592ebedd93ecab9a4b188fb0051e454f819
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-16 06:54:06 +0000

    update docs/build at 1434437646. [ci skip]

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:asdf-roswell)
+ :documents ((:title "ASDF-ROSWELL"
+              :authors ("Rudolph Miller")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
