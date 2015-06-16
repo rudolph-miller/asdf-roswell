2b696697f02e98bbfe6dfab90e812d77f9c5b3d4
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-16 15:51:33 +0900

    change base class of ros to cl-source-file.

--- src/asdf-roswell.lisp
+++ src/asdf-roswell.lisp
@@ -3,21 +3,7 @@
   (:use :cl :asdf))
 (in-package :asdf-roswell)
 
-(defclass ros (source-file)
+(defclass ros (cl-source-file)
   ((type :initform "ros")))
-
-(defmethod perform ((op load-op) (component ros))
-  (declare (ignore op))
-  #+ros.init
-  (load (compile-file-pathname* (component-pathname component)))
-  #-ros.init
-  (warning "roswell/init.lisp is not loaded."))
-
-(defmethod perform ((op compile-op) (component ros))
-  (declare (ignore op))
-  #+ros.init
-  (compile-file* (component-pathname component))
-  #-ros.init
-  (warning "roswell/init.lisp is not loaded."))
 
 (import 'ros :asdf)
